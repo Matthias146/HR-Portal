276ea71c47d6f8727f0ce1808a4718342e4b67bc
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-02-22 10:48:23 +0100

    feat(employees): implement modern control flow table and enterprise data grid styling

--- src/app/features/employees/employees.ts
+++ src/app/features/employees/employees.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Employee } from './employee.model';
 
 @Component({
   selector: 'app-employees',
@@ -7,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './employees.scss',
 })
 export class Employees {
-
+  employeeList = signal<Employee[]>([
+    {
+      id: 1001,
+      firstName: 'Max',
+      lastName: 'Mustermann',
+      department: 'IT',
+      role: 'MANAGER',
+      status: 'PRESENT',
+    },
+    {
+      id: 1002,
+      firstName: 'Peter',
+      lastName: 'Lustig',
+      department: 'Personal',
+      role: 'ADMIN',
+      status: 'PRESENT',
+    },
+    {
+      id: 1003,
+      firstName: 'Sebastian',
+      lastName: 'Seeberger',
+      department: 'Lager',
+      role: 'USER',
+      status: 'PRESENT',
+    },
+  ]);
 }
